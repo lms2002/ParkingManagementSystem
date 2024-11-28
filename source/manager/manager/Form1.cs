d6f133bdfa39ba4330e5926b2a8e02d6e22c91c6
GyuCastle <djdjdhdjdhdje@gmail.com>
2024-11-29 02:17:56 +0900

    Vehicle manager add

---
+++ source/manager/manager/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace manager
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connectionString = "User Id=ParkingAdmin; Password=1111; Data Source=(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = xe)))";
+        private string commandMode; // 추가, 삭제, 수정 모드
+        private int selectedVehicleId; // 선택된 Vehicle ID
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataGrid();
+        }
+        private void LoadDataGrid()
+        {
+            try
+            {
+                using (var connection = new OracleConnection(connectionString))
+                {
+                    connection.Open();
+                    var query = "SELECT * FROM Vehicle ORDER BY vehicle_id";
+                    var adapter = new OracleDataAdapter(query, connection);
+                    var dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridView1.DataSource = dataTable;
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                    dataGridView1.AllowUserToAddRows = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"데이터 로드 중 오류 발생: {ex.Message}");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string lastFourDigits = textBox1.Text.Trim();
+            if (string.IsNullOrWhiteSpace(lastFourDigits))
+            {
+                MessageBox.Show("Please enter the last 4 digits of the vehicle number.");
+                return;
+            }
+
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Vehicle WHERE SUBSTR(vehicle_number, -4) = :lastFourDigits";
+                    OracleCommand command = new OracleCommand(query, connection);
+                    command.Parameters.Add(new OracleParameter(":lastFourDigits", lastFourDigits));
+
+                    OracleDataAdapter adapter = new OracleDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridView1.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error searching data: " + ex.Message);
+                }
+            }
+        }
+
+        private void 차량추가ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commandMode = "추가";
+            using (var form2 = new Form2(connectionString, commandMode))
+            {
+                form2.ShowDialog();
+            }
+            LoadDataGrid(); // 데이터 새로고침
+        }
+
+        private void 차량삭제ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("삭제할 차량을 선택하세요.");
+                return;
+            }
+
+            selectedVehicleId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["vehicle_id"].Value);
+            commandMode = "삭제";
+
+            using (var form2 = new Form2(connectionString, commandMode, selectedVehicleId))
+            {
+                form2.ShowDialog();
+            }
+            LoadDataGrid(); // 데이터 새로고침
+        }
+
+        private void 차량수정ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("수정할 차량을 선택하세요.");
+                return;
+            }
+
+            selectedVehicleId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["vehicle_id"].Value);
+            commandMode = "수정";
+
+            using (var form2 = new Form2(connectionString, commandMode, selectedVehicleId))
+            {
+                form2.ShowDialog();
+            }
+            LoadDataGrid(); // 데이터 새로고침
+        }
+    }
+}
---
+++ source/manager/manager/Form2.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace manager
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "vehicle_id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "vehicle_number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "vehicle_type";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(395, 123);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 21);
+            this.txtId.TabIndex = 3;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(395, 170);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 21);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(395, 215);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(100, 21);
+            this.txtType.TabIndex = 5;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(301, 281);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(122, 69);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "확인";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
