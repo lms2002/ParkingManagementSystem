649e0a5fb20170f31e7f58420147c30760265998
GyuCastle <86282616+GyuCastle@users.noreply.github.com>
2024-12-05 14:51:10 +0900

    StoreManager add

---
+++ source/manager/manager/StoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace manager
+{
+    public partial class StoreManager : Form
+    {
+        public StoreManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
